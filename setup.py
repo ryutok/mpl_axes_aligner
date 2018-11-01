c6192f883ca337b30fab21cb046d3ae781f1e39a
Ryuto-MBA <26628494+ryutok@users.noreply.github.com>
2018-11-02 00:14:21 +0900

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mpl_axes_aligner',
+      version='0.1',
+      author='ryutok',
+      # author_email='',
+      url='https://github.com/ryutok/mpl_axes_aligner',
+      description=('Adjust the plotting range of matplotlib.axes.Axes '
+                   'objects to align the origins with the position'),
+      # long_description=,
+      # license=mit,
+      py_modules=['axes_aligner'],
+      )
