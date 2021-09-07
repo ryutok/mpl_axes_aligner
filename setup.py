0260a6904fc6d492e63c5361b8ed431ed63ba6ff
Ryuto <26628494+ryutok@users.noreply.github.com>
2021-09-07 09:51:53 +0900

    Bug fixed.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='mpl_axes_aligner',
-    version='1.1',
+    version='1.2',
     author='ryutok',
     author_email='ryutokm+os@gmail.com',
     url='https://github.com/ryutok/mpl_axes_aligner',
